de9e558621371b42931b9146f782f6726e694265
Denatkins <dennisodibo@yahoo.com>
2022-10-11 06:33:41 -0700

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
-        try:
-            print(my_list[m], end="")
-         n += 1
+    try:
+        for i in my_list[:x]:
+            print(my_list[i-1], end="")
+            print()
+            return my_list[x-1]
         except IndexError:
-            break
-        print()
-        return n
+            return my_list[-1]
