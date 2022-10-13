7ee1d8f7047f18965dde1fe5676feed4c2c73a49
Denatkins <dennisodibo@yahoo.com>
2022-10-13 10:08:15 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-# 0-square.py by Dennis Odibo
-"""Defines a square """
+"""Square class definition"""
 
 
 class Square:
-    """Represents a square"""
-
-    def __init__(self, size):
-        """Initializing this square class
-    Args: size - represnets the size of the square defined
+    """Represents a square
+    Attributes:
+    __size (int): size of a side of the square
     """
-
-    self.__size = size
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+        size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
