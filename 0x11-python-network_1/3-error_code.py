08788f868d36351dadd4b5dbf0b279016c02449f
Denatkins <dennisodibo@yahoo.com>
2023-01-17 14:56:16 -0800

    Update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,7 +11,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
